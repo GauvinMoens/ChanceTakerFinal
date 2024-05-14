d565eb054e414002c5e6aded4ede25315e05ea0b
Gauvin <gauvin.moens@student.howest.be>
2024-05-14 14:58:07 +0200

    Collider fix

--- Assets/Scripts/PowerUpP1.cs
+++ Assets/Scripts/PowerUpP1.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class PowerUpP1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject[] powerUp;
+    [SerializeField] Material[] powerUpMat;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
